7203570ad5dc15fa7cd9b8e5702a08bfefb9d98f
petermgoh <45682969+petermgoh@users.noreply.github.com>
2025-01-11 15:15:43 -0600

    Controller and Errorhandling

--- app.js
+++ app.js
@@ -9,7 +9,19 @@ app.use("/author", authorRouter);
 app.use("/books", bookRouter);
 app.use("/", indexRouter);
 
+
+  
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`My first express app - listening on port ${PORT}!`);
 });
+
+// Every thrown error in the application or the previous middleware 
+// function calling `next` with an error as an argument will eventually 
+// go to this middleware function
+app.use((err, req, res, next) => {
+    console.error(err);
+    // We can now specify the `err.statusCode` that exists in our custom error class and if it does not exist it's probably an internal server error
+    res.status(err.statusCode || 500).send(err.message);
+});
+  
---
+++ controllers/authorController.js
@@ -0,0 +1,19 @@
+// controllers/authorController.js
+
+const db = require("../db");
+const asyncHandler = require("express-async-handler");
+const CustomNotFoundError = require("../errors/customNotFoundError");
+
+const getAuthorById =  asyncHandler(async (req, res) => {
+  const { authorId } = req.params;
+
+  const author = await db.getAuthorById(Number(authorId));
+
+  if (!author) {
+    throw new CustomNotFoundError("Author not found");
+  }
+
+  res.send(`Author Name: ${author.name}`);
+});
+
+module.exports = { getAuthorById };
